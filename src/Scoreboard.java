559b78e37a7fb95ed7098f00a0b37a346bdfa920
mohammadiriaz <mohammadr95@nycstudents.net>
2025-12-11 12:16:13 -0500

    Added constructor and getscore method

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,4 +1,19 @@
-public class Scoreboard 
+public class Scoreboard
 {
-  
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;
+    }
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
 }
